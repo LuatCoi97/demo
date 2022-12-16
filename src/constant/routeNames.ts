57b23018f919b294456bb900b7f7da725d2f594f
moderate3012 <moderate@creatorchain.network>
2022-12-16 17:43:07 +0700

    init

---
+++ src/constant/routeNames.ts
@@ -0,0 +1,13 @@
+export enum RouteName {
+  activityView = '/activity',
+  auctionsView = '/auctions',
+  home = '/',
+  marketplace = '/marketplace',
+  marketplaceMint = '/marketplace/:tokenMint',
+  sell = '/sell',
+  drop = '/drop',
+  customToken = '/custom-token-marketplace',
+  multipleCollection = '/multi-collection-marketplace',
+  marketplaceWithUrl = '/marketplace-with-url',
+  demo = '/demo',
+}
